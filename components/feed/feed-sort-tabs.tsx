f36967d4aaa75002d1f22d0f05b1aa1966c315f1
wlewis0991 <wlewis1990@gmail.com>
2026-06-02 14:51:31 -0700

    Sort tabs added to UI unstyled and unworking

--- components/feed/feed-sort-tabs.tsx
+++ components/feed/feed-sort-tabs.tsx
@@ -1,3 +1,33 @@
-export function FeedSortTabs() {
-  return <div>FeedSortTabs</div>;
+import { FeedSort } from "@/lib/types";
+import { Flame, Sparkles, TrendingUp } from "lucide-react";
+import Link from "next/link";
+
+export function FeedSortTabs({
+  current,
+  tag,
+}: {
+  current: FeedSort;
+  tag?: string;
+}) {
+  const tabs: { id: FeedSort; label: string; icon: typeof Flame }[] = [
+    { id: "hot", label: "Hot", icon: Flame },
+    { id: "new", label: "New", icon: Sparkles },
+    { id: "top", label: "Top", icon: TrendingUp },
+  ];
+  return (
+    <div>
+      <div>
+        {tabs.map(({ id, label, icon: Icon }) => {
+          const active = current === id;
+
+          return (
+            <Link key={id} href={``}>
+              <Icon />
+              {label}
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
