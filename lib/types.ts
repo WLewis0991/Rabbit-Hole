4a80dba54e03d1bf426a07ec66cce1f880dc2aea
wlewis0991 <wlewis1990@gmail.com>
2026-06-02 09:44:17 -0700

    Functions to query and sort posts created

--- lib/types.ts
+++ lib/types.ts
@@ -4,3 +4,15 @@ export type User = {
   displayName?: string;
   avatarUrl?: string;
 };
+
+export type FeedSort = "hot" | "new" | "top";
+
+export type Post = {
+  id: string;
+  authorId: string;
+  title: string;
+  body: string;
+  tagSlugs: string[];
+  createdAt: string;
+  commentCount: number;
+};
